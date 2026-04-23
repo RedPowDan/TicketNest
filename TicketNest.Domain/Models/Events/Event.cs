1af7e22ceb47de0cae1a8d238b2d3f8e764a9fde
Danil Chernyh <da.chernykh@rambler-co.ru>
2026-04-23 08:20:39 +0300

    #1 Метод создания события, валидация

--- TicketNest.Domain/Models/Events/Event.cs
+++ TicketNest.Domain/Models/Events/Event.cs
@@ -1,4 +1,5 @@
 ﻿using TicketNest.Domain.ValueObjects;
+using TicketNest.Shared.Objects;
 
 namespace TicketNest.Domain.Models.Events;
 
@@ -31,5 +32,35 @@ public class Event
     public static Event LoadFromStorage(EventId id, EventTitle title, EventDescription? description, DateTime startAt, DateTime endAt)
     {
         return new Event(id, title, description, startAt, endAt);
+    }
+
+    public static Result<Event, string> Create(EventTitle title, EventDescription? description, DateTime startAt, DateTime endAt)
+    {
+        if (CanCreate(title, description, startAt, endAt) is { IsSuccess: true } result)
+        {
+            return result.Error;
+        }
+
+        return new Event(EventId.New(), title, description, startAt, endAt);
+    }
+
+    private static UnitResult<string> CanCreate(EventTitle title, EventDescription? description, DateTime startAt, DateTime endAt)
+    {
+        if (title == null)
+        {
+            return "Название события не должно быть пустое";
+        }
+
+        if (startAt == default)
+        {
+            return "Начало события не может быть значением по умолчанию";
+        }
+
+        if (startAt > endAt)
+        {
+            return "Начало события не может быть больше чем его окончание";
+        }
+
+        return UnitResult<string>.FromSuccess();
     }
 }
---
+++ TicketNest.Shared/Objects/UnitResult.cs
@@ -0,0 +1,18 @@
+﻿namespace TicketNest.Shared.Objects;
+
+public sealed class UnitResult<TError> : ResultBase<TError>
+{
+    private UnitResult()
+    {
+    }
+
+    private UnitResult(TError error) : base(error)
+    {
+    }
+
+    public static UnitResult<TError> FromSuccess() => new();
+
+    public static UnitResult<TError> FromFailure(TError error) => new(error);
+
+    public static implicit operator UnitResult<TError>(TError error) => FromFailure(error);
+}
