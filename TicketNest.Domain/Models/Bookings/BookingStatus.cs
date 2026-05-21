42a98b26faeb42481cecd72918dfb52c336d1959
Danil Chernyh <da.chernykh@rambler-co.ru>
2026-05-21 07:09:55 +0300

    #3 Доработка доменного слоя

---
+++ TicketNest.Domain/Models/Bookings/BookingStatus.cs
@@ -0,0 +1,22 @@
+﻿namespace TicketNest.Domain.Models.Bookings;
+
+/// <summary>
+/// Статус бронирования
+/// </summary>
+public enum BookingStatus
+{
+    /// <summary>
+    /// Бронь создана, ожидает обработки
+    /// </summary>
+    Pending = 0,
+
+    /// <summary>
+    /// Бронь подтверждена
+    /// </summary>
+    Confirmed = 1,
+
+    /// <summary>
+    /// Бронь отклонена
+    /// </summary>
+    Rejected = 2
+}
