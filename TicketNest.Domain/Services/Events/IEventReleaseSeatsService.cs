cbdbc7253ec2d619fa7a67ae6ae91bc444e3defd
Danil Chernyh <da.chernykh@rambler-co.ru>
2026-08-23 18:24:18 +0300

    #9 Удаление старого кода

--- TicketNest.Domain/Services/Events/IEventReleaseSeatsService.cs
+++
@@ -1,9 +0,0 @@
-﻿using TicketNest.Domain.Models;
-using TicketNest.Shared.Objects;
-
-namespace TicketNest.Domain.Services.Events;
-
-public interface IEventReleaseSeatsService
-{
-    Task<UnitResult<Error>> ReleaseSeats(Guid bookingId, int count = 1, CancellationToken ct = default);
-}
